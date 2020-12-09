9f28ef5aa0ba210bdfce5add1fdb109aa68cdd3d
Hargas Andrea <hargas.andrea@gmail.com>
2020-12-09 13:47:06 +0100

    Mobile nav buttons and icons style complete

---
+++ js/header.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $(".menu-btn").click(function(){
+        $(".menu-icon").toggleClass("open")
+    });
+
+    $(".contact-btn").click(function(){
+        $(".contact-icon").toggleClass("open")
+    });
+
+})
+
+
+
+
+
+
+
+
