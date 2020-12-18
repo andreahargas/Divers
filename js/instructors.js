5bfcbea2bd5ae4898c4855f966674d855ed4b4b4
Hargas Andrea <hargas.andrea@gmail.com>
2020-12-18 20:31:23 +0100

    Section Instructors complete - mobile

---
+++ js/instructors.js
@@ -0,0 +1,20 @@
+$(".instructors-slider-button").each(function(){
+
+    $(this).on("click", function(){
+
+        let activeButton = $(this);
+    
+        $(".instructors-slider-button").removeClass("active")
+        activeButton.addClass("active");     
+
+        $(".instructors-item").removeClass("active");      
+        $(".instructors-item").eq(activeButton.index()).addClass("active").removeClass("prev next");
+
+        let activeInstructor = $(".instructors-item.active");
+        let prevInstructor = activeInstructor.prevAll()
+        let nextInstructor = activeInstructor.nextAll()
+
+        prevInstructor.removeClass("next").addClass("prev")
+        nextInstructor.removeClass("prev").addClass("next")  
+    })
+})
