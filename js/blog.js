abec1721f9725d9a6dcedb4d736848e3a954dd5c
Hargas Andrea <hargas.andrea@gmail.com>
2020-12-19 10:01:57 +0100

    Section Blog complete - mobile

---
+++ js/blog.js
@@ -0,0 +1,20 @@
+$(".blog-slider-button").each(function(){
+
+    $(this).on("click", function(){
+
+        let activeButton = $(this);
+    
+        $(".blog-slider-button").removeClass("active")
+        activeButton.addClass("active");     
+
+        $(".blog-item").removeClass("active");      
+        $(".blog-item").eq(activeButton.index()).addClass("active").removeClass("prev next");
+
+        let activeBlog = $(".blog-item.active");
+        let prevBlog = activeBlog.prevAll()
+        let nextBlog = activeBlog.nextAll()
+
+        prevBlog.removeClass("next").addClass("prev")
+        nextBlog.removeClass("prev").addClass("next")  
+    })
+})
